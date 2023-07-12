797fa7af95e262ece4fb8f8a00e33683d6f9b2f0
Avishek255874 <avisheksharma6290@gmail.com>
2023-07-12 17:30:33 +0530

    third commmit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/NeeedCrypto/', // Update this with your repository name
-});
+  plugins: [react()],
+})
